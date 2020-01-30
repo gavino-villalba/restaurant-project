afad7d09a696d64cfdfab91273f9d6a89e93607a
gavino-villalba <gavinovillalba@gmail.com>
2020-01-30 00:37:56 -0600

    gavino
    cambios

--- js/principal.js
+++ js/principal.js
@@ -1,11 +1,7 @@
  $(document).ready(function ($) {
+   $('#fecha_reserva').datepicker({
+        language: 'pt-BR'
+     });
 
 
-
-   $("#fecha_reserva").datepicker({
-                        format:"dd/mm/yyyy",
-                        language: "es",
-                        startDate: "' . date('d/m/Y', strtotime(min($arrFechas))) . '",
-                        endDate: "' . date('d/m/Y', strtotime(max($arrFechas))) . '"
-                    });
 });
