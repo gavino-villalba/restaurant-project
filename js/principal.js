86bb4de70bb030c1adbfb32e0bb11441d3fed8a9
gavino-villalba <gavinovillalba@gmail.com>
2020-01-31 02:28:07 -0600

    gavinovillalba@gmail.com
    modificaciones medianoche

--- js/principal.js
+++ js/principal.js
@@ -4,4 +4,12 @@
      });
 
 
+  var horas=0;
+  var hora_min=9;
+  var hora_max=20;
+
+  for(var i=hora_min; i<hora_max; i++){
+    $("#timeReserva").append('<option value='+i+'>'+i+':00 Hrs</option>');
+  }
+
 });
