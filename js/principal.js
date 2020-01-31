c858b61db79361972658ba342116c19ba568c2a7
gavino <gavino.villalba@s2credit.com>
2020-01-31 14:01:34 -0600

    actualizacion

--- js/principal.js
+++ js/principal.js
@@ -12,4 +12,43 @@
     $("#timeReserva").append('<option value='+i+'>'+i+':00 Hrs</option>');
   }
 
+  var contador=1;
+
+  // $(document).on("click",function(){
+  //   // if(contador ==0){
+  //   //   $(".menu_cabecera").animate({
+  //   //     left: '-100%'
+  //   //   });
+  //   //   //contador=0;
+  //   // }
+  //
+  //
+  // })
+
+  $(".menu_bar").click(function(){
+    if(contador ==1){
+      $(".menu_cabecera").animate({
+        left: '0'
+      });
+      contador=0;
+    }else{
+      $(".menu_cabecera").animate({
+        left: '-100%'
+      });
+      contador=1;
+    }
+  });
+
+  $(".go_to").click(function(){
+      $(".menu_cabecera").animate({
+        left: '-100%'
+      });
+      contador=1;
+    
+  });
+
+
+
+
+
 });
